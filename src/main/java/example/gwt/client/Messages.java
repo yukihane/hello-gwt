894302b3c243cc385e7f076bd2dd7643f58d45f4
yukihane <yukihane.feather@gmail.com>
2015-09-04 02:41:30 +0900

    auto-generated files

---
+++ src/main/java/example/gwt/client/Messages.java
@@ -0,0 +1,10 @@
+package example.gwt.client;
+
+public interface Messages extends com.google.gwt.i18n.client.Messages {
+  
+  @DefaultMessage("Enter your name")
+  String nameField();
+
+  @DefaultMessage("Send")
+  String sendButton();
+}
